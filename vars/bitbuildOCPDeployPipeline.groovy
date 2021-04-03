b724d374ffa970d6de8a5a466353b8c088dc2da1
hholtzhausen <hannes.holtzhausen@9thbit.co.za>
2021-04-03 14:13:13 +0200

    OCP pipeline

---
+++ vars/bitbuildOCPDeployPipeline.groovy
@@ -0,0 +1,34 @@
+//def build_agent = 'bitbuild-maven-oci'
+//def mvn_settings = 'bitbuild-maven-settings'
+//def oc_creds_prefix = 'bitbuild-oc-login'
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+pipeline {
+  agent {
+    label pipelineParams.build_agent
+  }
+
+  environment {
+    ENV_PROFILE = bitbuildUtil.getEnvProfile(BRANCH_NAME)
+    MVN_SETTINGS_XML = credentials("${pipelineParams.mvn_settings}")
+    OC_CREDS_ID = "${pipelineParams.oc_creds_prefix}-${env.ENV_PROFILE}"
+  }
+
+  stages {
+    stage ('Apply Openshift Resources') {
+      environment {
+        OC_CREDS = credentials("${OC_CREDS_ID}")
+      }
+      steps {
+        sh 'mvn -s $MVN_SETTINGS_XML -Doc-user=$OC_CREDS_USR -Doc-password=$OC_CREDS_PSW -P$ENV_PROFILE,ocp-deployment'
+      }
+    }
+  }
+}
+}
