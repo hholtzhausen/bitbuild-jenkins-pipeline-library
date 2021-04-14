ec62c9b24149f2014129c27df82eeef62f35e000
hholtzhausen <hannes.holtzhausen@9thbit.co.za>
2021-04-14 20:04:20 +0200

    Added checkout step

--- vars/bitbuildMavenContainerPipeline.groovy
+++ vars/bitbuildMavenContainerPipeline.groovy
@@ -25,6 +25,7 @@ pipeline {
   stages {
     stage ('Init Build') {
       steps {
+        checkout scm
         script {
           mvnArgs = bitbuildUtil.getChangeSetDirs(currentBuild.changeSets)
         }
