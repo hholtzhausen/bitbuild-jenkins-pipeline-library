69888e510d962653d4798e4f2c34b7d9d344a8d5
hholtzhausen <hannes.holtzhausen@9thbit.co.za>
2021-04-14 20:26:30 +0200

    Return csv string of directories

--- vars/bitbuildUtil.groovy
+++ vars/bitbuildUtil.groovy
@@ -12,22 +12,22 @@ def getEnvProfile(branchName) {
 
 def getChangeSetDirs(changeLogSets) {
 
-  def output = ""
+  Set output 
 
   for (int i = 0; i < changeLogSets.size(); i++) {
     def entries = changeLogSets[i].items
     for (int j = 0; j < entries.length; j++) {
       def entry = entries[j]
-      echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+      //echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
       def files = new ArrayList(entry.affectedFiles)
       for (int k = 0; k < files.size(); k++) {
-            def file = files[k]
-            //echo "  ${file.editType.name} ${file.path}"
-            if(!file.path.startsWith("vars"))
-              output += "${file.path},"
+            def filePath = files[k].path
+            if(!filePath.startsWith("vars"))
+              output.add(filePath.substring(0,filePath.firstIndexOf("/")))
       }
     }
   }
 
   echo "${output}"
+  return output.join(",")
 }
