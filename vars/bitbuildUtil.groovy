bc1de5e1bb67967935cc3d463c6ead52af84e750
hholtzhausen <hannes.holtzhausen@9thbit.co.za>
2021-04-14 19:43:58 +0200

    missing closing bracket

--- vars/bitbuildUtil.groovy
+++ vars/bitbuildUtil.groovy
@@ -17,15 +17,16 @@ def getChangeSetDirs(changeLogSets) {
   for (int i = 0; i < changeLogSets.size(); i++) {
     def entries = changeLogSets[i].items
     for (int j = 0; j < entries.length; j++) {
-        def entry = entries[j]
-        echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-        def files = new ArrayList(entry.affectedFiles)
-        for (int k = 0; k < files.size(); k++) {
+      def entry = entries[j]
+      echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+      def files = new ArrayList(entry.affectedFiles)
+      for (int k = 0; k < files.size(); k++) {
             def file = files[k]
             //echo "  ${file.editType.name} ${file.path}"
             output =+ "${file.path},"
-        }
+      }
     }
+  }
 
-    echo "${ouput}"
+  echo "${ouput}"
 }
